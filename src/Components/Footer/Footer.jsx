2624cc77616d6e91e9636ea11e264647aa40d769
Aditya Kishor Dahal <aditya.kishor.dahal@maitriservices.com>
2024-08-16 21:28:05 +0545

    Initial Commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='py-3'>
+        <p className='mb-0'>
+        Just a Call away! Call us: 98 080 070 00 00
+        </p>
+    </div>
+  )
+}
+
+export default Footer;
